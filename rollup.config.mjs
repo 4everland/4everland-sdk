2181a42b9ac1fa1cec57d186c5defff6e0e7de62
yepCing <yepCing@gmail.com>
2023-08-31 16:34:59 +0800

    chore: update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,8 +8,8 @@ import terser from '@rollup/plugin-terser'
 import nodePolyfills from 'rollup-plugin-polyfill-node'
 import replace from '@rollup/plugin-replace'
 
-// const isProduction = process.env.NODE_ENV === 'production'
-// console.log(isProduction)
+const isProduction = process.env.NODE_ENV === 'production'
+console.log(isProduction)
 const pkg = 'foreverland'
 const extensions = ['.ts']
 export default {
@@ -34,11 +34,7 @@ export default {
       file: `./dist/${pkg}.umd.min.js`,
       format: 'umd',
       name: pkg,
-      plugins: [terser()],
-      globals: {
-        S3: 'S3',
-        Upload: 'Upload'
-      }
+      plugins: [terser()]
     }
   ],
   plugins: [
